f838b37833ff2bb97821f528e695b1021af367a3
JoeSephian <joe.alex.hill@gmail.com>
2023-06-14 16:28:26 +0100

    fix issue with router declarations in multiple places

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import Header from "./Components/Header";
 import SinglePark from "./Components/SinglePark";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import ParksList from "./Components/ParksList";
 
 function App() {
   return (
     <>
       <Header />
-      <BrowserRouter>
         <Routes>
           <Route path="/parks" element={<ParksList />} />
           <Route path="/parks/:park_id" element={<SinglePark />}></Route>
         </Routes>
-      </BrowserRouter>
     </>
   );
 }
