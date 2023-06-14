4a3b8c92a8596a9605851a49bbd2ea4348f833b7
Callum Neale <callum@Callums-MacBook-Air.local>
2023-06-14 11:41:57 +0100

    Creates /parks endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import Header from "./Components/Header"
 import ParksList from "./Components/ParksList";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <>
       <Header />
-      <ParksList/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/parks" element={<ParksList />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
+
+
 
 export default App;
--- src/Components/ParksList.tsx
+++ src/Components/ParksList.tsx
@@ -1,31 +1,36 @@
 import server from "../Api/api";
 import { useEffect, useState } from "react";
 import ParksListCard from "./ParksListCard";
-import {Park} from "../../types/CustomTypes"
+import { Park } from "../../types/CustomTypes";
 
+function ParksList() {
+  const [parks, setParks] = useState<Park[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
+  useEffect(() => {
+    server.get(`/parks`).then(({ data }) => {
+      setParks(data);
+      setIsLoading(false);
+    });
+  }, []);
 
-  
-
-
-function ParksList(){
-    const [parks, setParks] = useState<Park[]>([])
-
-    useEffect(() => {
-        server.get(`/parks`).then(({ data } ) => {
-            setParks(data)
-        });
-    }, [])
-
-
-    return (<>
-        <h2>All Parks</h2>
- 
-       
-        <ParksListCard parks={parks}/>
-       
-        
-        
-        </>)
+  return (
+    <>
+      <h2>All Parks</h2>
+      {isLoading ? (
+        <h3>Loading...</h3>
+      ) : (
+        <ul>
+          {parks.map((park) => {
+            return (
+              <li key={park.id}>
+                <ParksListCard park={park} />
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </>
+  );
 }
 export default ParksList;
