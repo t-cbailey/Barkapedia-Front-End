76b25f3434469d568e1a65c500f0a21bda7ecb09
Dafydd <dafydd@Dafydds-MacBook-Air.local>
2023-06-16 16:53:02 +0100

    drafted connection function

--- utils/Firebase.ts
+++ utils/Firebase.ts
@@ -26,6 +26,21 @@ if (process.env.NODE_ENV !== 'production') {
   connectAuthEmulator(auth, "http://127.0.0.1:9099");
 }
 
+//   To get the below to work, make sure that you have the firebase emulator running and listen.ts running
+// From there, go a POST to http://localhost:9191/api/users/ with the below ...
+// {
+//   "email": "joe@example.com",
+//   "username": "JoeB",
+//   "password": "abc1232",
+//   "type": "consumer"
+// }
+// ... from after this, the auth error should change to be "signed in user_11"
+
+// import { firebaseSignIn } from "utils/Firebase";
+// const email = "joe@example.com";
+// const password = "abc1232";
+// firebaseSignIn(email, password)
+
 export const firebaseSignIn = (email: string, password: string): void => {
   signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
