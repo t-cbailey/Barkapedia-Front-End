cc71729414935a23ee39b586cdca19d274ebe271
Dafydd <dafydd@Dafydds-MacBook-Air.local>
2023-06-21 07:45:58 +0100

    Veri and biz icons

--- src/Components/SingleReview.tsx
+++ src/Components/SingleReview.tsx
@@ -1,20 +1,49 @@
-
 import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
 import CardContent from "@mui/material/CardContent";
+import CardHeader from "@mui/material/CardHeader";
 import CardActions from "@mui/material/CardActions";
 import Typography from "@mui/material/Typography";
-import { Review, SingleReviewProps } from "../types/CustomTypes";
-import '../Styles/styles.css'
+import { SingleReviewProps, User } from "../types/CustomTypes";
+import "../Styles/styles.css";
+import { Box } from "@mui/material";
+
+import VerifiedIcon from "@mui/icons-material/Verified";
+import BusinessIcon from '@mui/icons-material/Business';
+import { useState, useEffect } from "react";
+import getUserByID from "../utils/getUserByID.utils";
 
 export default function SingleReview({ review, fullWidth }: SingleReviewProps) {
-    if (!review) {
-        return <h3 className="loading">Loading...</h3>;
+  if (!review) {
+    return <h3 className="loading">Loading...</h3>;
+  }
+
+  const [userData, setUserData] = useState<User | undefined>();
+  const [showVerifiedIcon, setShowVerifiedIcon] = useState<boolean>(false);
+  const [showBusinessIcon, setShowBusinessIcon] = useState<boolean>(false);
+
+  useEffect(() => {
+    getUserByID(review.user_id).then((user) => setUserData(user));
+  }, []);
+
+  useEffect(() => {
+    if (userData) {
+      setShowVerifiedIcon(userData.isVerified);
+      setShowBusinessIcon(userData.type === "Business");
     }
-  
+  }, [userData]);
+
   return (
     <Card sx={{ maxWidth: fullWidth ? "100%" : 345 }}>
-      <CardHeader title={review.title} subheader={review.username} />
+      <CardHeader
+        title={review.title}
+        subheader={
+          <Box component="span" sx={{ display: "flex", alignItems: "center" }}>
+            {review.username}
+            {showVerifiedIcon && <VerifiedIcon aria-hidden={false} aria-label="Verified user" />}
+            {showBusinessIcon && <BusinessIcon aria-hidden={false} aria-label="Business User" />}
+          </Box>
+        }
+      />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
           {review.body}
