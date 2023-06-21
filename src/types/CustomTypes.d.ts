7a9bc29fe9d1e7153a94a6afbf952974dde0aa70
Dafydd <dafydd@Dafydds-MacBook-Air.local>
2023-06-21 12:30:06 +0100

    Basic upvote and down vote

--- src/types/CustomTypes.d.ts
+++ src/types/CustomTypes.d.ts
@@ -47,7 +47,7 @@ export interface User {
 }
 
 export interface Review {
-  review_id: string;
+  id: string;
   park_id: string;
   user_id: string;
   rating: number;
@@ -55,6 +55,10 @@ export interface Review {
   body: string;
   votes: number;
   username;
+}
+
+export interface PatchReviewResponse {
+  data: Review;
 }
 
 export interface orderObj {
@@ -136,3 +140,12 @@ export interface UserRequest {
   password: string;
   type: string;
 }
+
+export interface ReviewVoteRequest {
+  review_id: string;
+  increment: number;
+}
+
+
+
+type VoteValue = 1 | -1 | null;
