bcb52cbea6de2c9490151b2ef8f8229723a3fcfe
JoeSephian <joe.alex.hill@gmail.com>
2023-06-22 11:27:56 +0100

    add basic styling to No Reviews callout

--- src/Components/ParkReviews.tsx
+++ src/Components/ParkReviews.tsx
@@ -1,32 +1,37 @@
-
 import { ReviewProps } from "../types/CustomTypes";
 import SingleReview from "./SingleReview";
 import "../Styles/card-list.css";
-import "../Styles/styles.css"
+import "../Styles/styles.css";
+import { Card, Typography } from "@mui/material";
 
 export default function ParkReviews({ reviews, isLoading }: ReviewProps) {
   if (isLoading) {
     return <h3 className="loading">Loading...</h3>;
-
   }
 
   if (!reviews || reviews.length === 0) {
-    return <h3>No reviews available</h3>;
+    return (
+      <Card
+        sx={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: "5px" }}
+      >
+        <Typography>No reviews available</Typography>
+      </Card>
+    );
   }
 
   return (
     <>
-    {reviews.map((review, index) => {
-      if (review.body !== "") {
-        return (
-          <li className="card-list" key={index}>
-            <SingleReview review={review} fullWidth={true} />
-          </li>
-        );
-      } else {
-        return null;
-      }
-    })}
-  </>
+      {reviews.map((review, index) => {
+        if (review.body !== "") {
+          return (
+            <li className="card-list" key={index}>
+              <SingleReview review={review} fullWidth={true} />
+            </li>
+          );
+        } else {
+          return null;
+        }
+      })}
+    </>
   );
 }
